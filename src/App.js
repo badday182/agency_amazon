9abfee727ec03d81cee99fc5cd7de26f21c47820
berk <badday182@gmail.com>
2025-07-16 21:57:33 +0300

    clean code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import ReviewCard from "./components/ReviewCard/ReviewCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -9,7 +8,6 @@ import "swiper/css/autoplay";
 
 import "./App.css";
 
-// import required modules
 import {
   Navigation,
   Pagination,
@@ -27,49 +25,27 @@ export default function App() {
           cssMode={false}
           // cssMode={true}
           navigation={true}
-          slidesPerView={3}
+          slidesPerView={1}
           speed={500}
-          // spaceBetween={5}
-          // autoplay={{
-          //   delay: 3000,
-          //   disableOnInteraction: false,
-          //   pauseOnMouseEnter: true,
-          // }}
+          spaceBetween={200}
           effect="slide"
-          // grabCursor={true}
-          // centeredSlides={false}
           loop={true}
           pagination={{
             clickable: true,
             dynamicBullets: true,
           }}
-          // navigation={{
-          //   nextEl: ".swiper-button-next",
-          //   prevEl: ".swiper-button-prev",
-          // }}
           modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
           className="mySwiper"
           breakpoints={{
-            320: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-            640: {
-              slidesPerView: 2,
-              spaceBetween: 30,
-            },
             768: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            1080: {
               slidesPerView: 3,
               spaceBetween: 10,
             },
           }}
-
-          // navigation={true}
-          // pagination={true}
-          // mousewheel={true}
-          // keyboard={true}
-          // modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-          // className="mySwiper"
         >
           <SwiperSlide>
             <ReviewCard />
