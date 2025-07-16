fceb9fd01e50b0baf27274b08d3d4f8843ff1f95
berk <badday182@gmail.com>
2025-07-16 10:23:27 +0300

    Add Space Grotesk font and update App component structure; remove unused logo images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <h1>Voices of Success with Sales Fortuna</h1>
+      <div className="cards_gallery">
+        <div className="arrow_back"></div>
+        <div className="cards"></div>
+        <div className="arrow_forward"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
