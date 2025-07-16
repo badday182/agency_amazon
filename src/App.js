a796b75b23da6dfa972f841063ddb13ceb82e328
berk <badday182@gmail.com>
2025-07-16 10:40:59 +0300

    Refactor App component to include review cards and reviewer section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,24 @@ function App() {
       <h1>Voices of Success with Sales Fortuna</h1>
       <div className="cards_gallery">
         <div className="arrow_back"></div>
-        <div className="cards"></div>
+        <div className="review-cards">
+          <div className="review-card">
+            <div className="card_image"></div>
+            <p>
+              Sales Fortuna made managing sales easier and helped us focus on
+              customers. Its tools have been crucial for our growth and client
+              satisfaction.
+            </p>
+          </div>
+          <div className="reviewer_section">
+            <div className="reviewer_section__reviewer-photo"></div>
+            <div className="reviewer__info reviewer"></div>
+            <p className="reviewer_name">Ethan Morgan</p>
+            <p className="reviewer_position">
+              Founder and CEO, Serene Living Products
+            </p>
+          </div>
+        </div>
         <div className="arrow_forward"></div>
       </div>
     </div>
